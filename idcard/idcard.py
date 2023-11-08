4ef258d65dba2dfd3635047f74740c110c73f38a
Ngô Văn Cảnh <canh25xp@gmail.com>
2023-11-08 14:42:33 +0700

    use logger instead of print

--- idcard/idcard.py
+++ idcard/idcard.py
@@ -237,7 +237,7 @@ def run(
     t = tuple(x.t / seen * 1E3 for x in dt)  # speeds per image
     LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
     total_end = time.time()
-    print(f"Total run time : {total_end - total_start} seconds")
+    LOGGER.info(f'Total run time : {total_end - total_start} seconds')
     if save_txt or save_img:
         s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
         LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
